99dd52f62c0b4d3d0979424784666fb1ff2d3ec2
monochrome <monochrome@gopurpose.cn>
2016-04-10 14:55:50 -0700

    add composer-install shell, remove common/modules/monochrome/request.bak

---
+++ composer-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
+php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === '7228c001f88bee97506740ef0888240bd8a760b046ee16db8f4095c0d8d525f2367663f22a46b48d072c816e7fe19959') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
+composer global require "fxp/composer-asset-plugin:~1.1.1"
+#END
